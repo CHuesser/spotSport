7489cf8df2c09822c25a05db9d1c1f4c249b2b88
CHuesser <cloe.huesser@students.fhnw.ch>
2017-09-16 20:24:45 +0200

    implement search

--- src/pages/participants/participants.ts
+++ src/pages/participants/participants.ts
@@ -12,6 +12,8 @@ import {UserServiceProvider} from "../../providers/user-service/user-service";
 export class ParticipantsPage {
 
   athletes : Athlete[] = [];
+  athAll : Athlete[] = [];
+  searchQuery: string = '';
 
   constructor(public navCtrl: NavController, private athleteServiceProvider:AthleteServiceProvider, private userServiceProvider:UserServiceProvider, private navParams:NavParams) {
     if(this.navParams.data) {
@@ -19,11 +21,27 @@ export class ParticipantsPage {
         var ath = JSON.parse(athletes._body)._embedded.athletes;
         ath.map((u) => u.user = this.userServiceProvider.getRandomUser())
         this.athletes = ath;
+        this.athAll = ath;
       });
     }else{
       var ath = this.navParams.data.athletes;
       ath.map((u) => u.user = this.userServiceProvider.getRandomUser())
       this.athletes = ath;
+      this.athAll = ath;
+    }
+  }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.athletes = this.athAll;
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.athletes = this.athletes.filter((athlete) => {
+        return (athlete.name.toLowerCase().indexOf(val.toLowerCase()) > -1) || (athlete.startNr.toString().indexOf(val.toLowerCase()) > -1);
+      })
     }
   }
   goToRunnerDetail(params){
