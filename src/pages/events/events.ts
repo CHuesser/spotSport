a485a21b3133a9387bb192f2fbf1eb93d968db5c
CHuesser <cloe.huesser@students.fhnw.ch>
2017-09-16 10:23:20 +0200

    remove unused imports

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ImagePage } from '../image/image';
-import { RunnerPage } from '../runner/runner';
-import { CameraPage } from '../camera/camera';
+
 
 @Component({
   selector: 'page-events',
